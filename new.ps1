9484a5e5f35bfa9a5ec0cb20d5dc6ae551d92d69
Dennis Riis <dr@driis.dk>
2024-12-01 18:52:32 +0100

    Initial commit - day 1

---
+++ new.ps1
@@ -0,0 +1,14 @@
+if ( $args.Length -lt 1) {
+    Write-Error "Supply a name for your new project"
+}
+$dir = $args[0]
+
+mkdir $dir
+Push-Location $dir
+dotnet new console 
+dotnet sln .. add .
+Remove-Item Program.cs
+Copy-Item ../template/*.cs . 
+dotnet build --no-restore
+
+Write-Host "$dir created, happy coding !"
