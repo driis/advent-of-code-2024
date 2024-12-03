6f514395b30676ca528d71828ee7677a4892e4c6
Dennis Riis <dr@driis.dk>
2024-12-03 20:27:05 +0100

    Less indexes, more iterators ... but better ?

--- day3/Program.cs
+++ day3/Program.cs
@@ -32,3 +32,20 @@ while (-1 != (cursor = data.IndexOf(Word(valid), StringComparison.Ordinal)))
     valid = !valid;
 }
 WriteLine(Multiplies(result));
+
+// Part 2 iterator
+IEnumerable<string> IncludedText(string text, bool includeState)
+{
+    string word = Word(includeState);
+    int until = text.IndexOf(word, StringComparison.Ordinal);
+    if (until == -1)
+        yield break;
+    if (includeState)
+        yield return text[..until];
+    
+    foreach (var next in IncludedText(text[(until + 2)..], !includeState))
+        yield return text;
+}
+
+string text = String.Join("", IncludedText(input, true));
+WriteLine(Multiplies(result));
