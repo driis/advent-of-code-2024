7b559da0d94a8b15258fdd4977ba7d10f22cecaf
Dennis Riis <dr@driis.dk>
2024-12-06 14:56:31 +0100

    fix day3 after overcomplicating it for 2 days

--- day3/Program.cs
+++ day3/Program.cs
@@ -17,35 +17,7 @@ int Multiplies(string data)
 WriteLine(Multiplies(input));
 
 // Part 2
-string data = String.Join("", input);
-int cursor;
-bool valid = true;
-string result = "";
-string Word(bool state) => state ? "don't()" : "do";
-while (-1 != (cursor = data.IndexOf(Word(valid), StringComparison.Ordinal)))
-{
-    if (valid)
-    {
-        result += data[..cursor];
-    }
-    data = data[..(cursor+2)];
-    valid = !valid;
-}
-WriteLine(Multiplies(result));
-
-// Part 2 iterator
-IEnumerable<string> IncludedText(string text, bool includeState)
-{
-    string word = Word(includeState);
-    int until = text.IndexOf(word, StringComparison.Ordinal);
-    if (until == -1)
-        yield break;
-    if (includeState)
-        yield return text[..until];
-    
-    foreach (var next in IncludedText(text[(until + 2)..], !includeState))
-        yield return text;
-}
-
-string text = String.Join("", IncludedText(input, true));
-WriteLine(Multiplies(result));
+var dos = input.Split("do()");
+dos = dos.Select(x => x.Split("don't()").First()).ToArray();
+string cleanedText = String.Join("", dos);
+WriteLine(Multiplies(cleanedText));
