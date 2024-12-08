6059c91b40107a61c93ca7ed0898660e4331dbf2
Dennis Riis <dr@driis.dk>
2024-12-08 17:26:17 +0100

    day8 part 1

---
+++ day8/Program.cs
@@ -0,0 +1,33 @@
+﻿// Normally we will start by reading lines from an input file
+var input = File.ReadAllLines(args.FirstOrDefault() ?? "input.txt");
+HashSet<Point> antinodes = new();
+var frequencies = input.SelectMany((data, y) =>
+    data.Select((f, x) => new { Frequency = f, At = new Point(x, y) }))
+    .Where(x => x.Frequency != '.')
+    .GroupBy(x => x.Frequency)
+    .ToArray();
+
+//frequencies.Select(f => new {f.Key, Locations = String.Join("; ", f.Select(fq => fq.At))}).DumpConsole();
+var h = input.Length;
+var w = input[0].Length;
+foreach (var frequency in frequencies)
+{
+    foreach (Point p in frequency.Select(x => x.At))
+    {
+        var otherPoints = frequency.Select(x => x.At).Where(op => op != p);
+        foreach (Point op in otherPoints)
+        {
+            int dx = op.X - p.X;
+            int dy = op.Y - p.Y;
+            var antinode = new Point(p.X - dx, p.Y - dy);
+            if (antinode is { Y: >= 0, X: >= 0 } && antinode.Y < h && antinode.X < w)
+            {
+                antinodes.Add(antinode);
+            }
+        }
+    }
+}
+
+WriteLine(antinodes.Count);
+
+record Point(int X, int Y);
