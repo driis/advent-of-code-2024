c50a10eda056ef7535540ab7833d052003e29497
Dennis Riis <dr@driis.dk>
2024-12-08 17:46:40 +0100

    day8 part 2, but wrong answer - both examples gives the right answer, unsure what's wrong here

--- day8/Program.cs
+++ day8/Program.cs
@@ -7,7 +7,7 @@ var frequencies = input.SelectMany((data, y) =>
     .GroupBy(x => x.Frequency)
     .ToArray();
 
-//frequencies.Select(f => new {f.Key, Locations = String.Join("; ", f.Select(fq => fq.At))}).DumpConsole();
+// Part 1
 var h = input.Length;
 var w = input[0].Length;
 foreach (var frequency in frequencies)
@@ -30,4 +30,29 @@ foreach (var frequency in frequencies)
 
 WriteLine(antinodes.Count);
 
+antinodes.Clear();
+// Part 2
+foreach (var frequency in frequencies)
+{
+    foreach (Point p in frequency.Select(x => x.At))
+    {
+        var otherPoints = frequency.Select(x => x.At).Where(op => op != p);
+        foreach (Point op in otherPoints)
+        {
+            int dx = op.X - p.X;
+            int dy = op.Y - p.Y;
+            var antinode = new Point(p.X - dx, p.Y - dy);
+            while(antinode is { Y: >= 0, X: >= 0 } && antinode.Y < h && antinode.X < w)
+            {
+                antinodes.Add(antinode);
+                antinodes.Add(op);
+                antinodes.Add(p);
+                antinode = new Point(antinode.X - dx, antinode.Y - dy);
+            }
+        }
+    }
+}
+
+antinodes.OrderBy(x => x.Y).ThenBy(x => x.X).DumpConsole();
+WriteLine(antinodes.Count);
 record Point(int X, int Y);
