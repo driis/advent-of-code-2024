576479f61e4469fad0753e2087989267c9d94286
Dennis Riis <dr@driis.dk>
2024-12-15 17:56:34 +0100

    day11 part 1

---
+++ day11/Program.cs
@@ -0,0 +1,37 @@
+﻿// Normally we will start by reading lines from an input file
+var input = File.ReadAllLines(args.FirstOrDefault() ?? "input.txt");
+string line = input.Single();
+
+var numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => x.ToLong()).ToArray();
+
+long[] ApplyRules(long[] state) => state.SelectMany(ApplyRulesToElement).ToArray();
+
+long[] ApplyRulesToElement(long element)
+{
+    if (element == 0)
+        return [1];
+    string str = element.ToString();
+    if (str.Length % 2 == 0)
+    {
+        int n = str.Length / 2;
+        return [str[..n].ToLong(), str[n..].ToLong()];
+    }
+
+    return [element * 2024];
+}
+
+const int blinks = 25;
+long sum = 0;
+foreach (var number in numbers)
+{
+    long[] temp = [number];
+    for (int n = 0; n < blinks; n++)
+    {
+        temp = ApplyRules(temp);
+    }
+
+    WriteLine($"{number} expanded to {temp.Length} stones.");
+    sum += temp.Length;
+}
+
+WriteLine(sum);
