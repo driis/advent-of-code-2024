51f581285efda553c45fd69adfbef59342b80f55
Dennis Riis <dr@driis.dk>
2025-01-03 20:52:56 +0100

    Day 14 part 2 silly sln, might want to reconsider this one

--- day14/Program.cs
+++ day14/Program.cs
@@ -16,11 +16,61 @@ quadrantsWithRobots.DumpConsole();
 int sf = quadrantsWithRobots.Where(x => x.Qudarant != -1).Select(x => x.Robots)
     .Aggregate((a, b) => a * b);
  WriteLine($"Part 1 Safety Factor {sf}");
+
+WriteLine("\nReady to simulate stages. Space to continue, ESC to quit");
+robots = input.Select(Robot.Parse).ToArray();
+
+void WaitForKey()
+{
+    var keyInfo = ReadKey(true);
+    if (keyInfo.Key == ConsoleKey.Escape)
+        throw new OperationCanceledException("ESC was hit");
+}
+
+bool IsChristmasTreeCandidate(Robot[] data)
+{
+    var quadrantsData = data.GroupBy(r => r.Quadrant)
+        .Select(g => new { Quadrant = g.Key, Row = g.Key / 3, Count = g.Count() })
+        .OrderBy(x => x.Quadrant)
+        .ToArray();
+    
+    var topBottomRatio = quadrantsData.Where(x => x.Row > 0).Sum(x => x.Count) * 1.0
+        / quadrantsData.Where(x => x.Row == 0).Sum(x => x.Count); 
+    
+    return topBottomRatio >= 2;
+}
+
+WaitForKey();
+for (int i = 0; i < 10000; i++)
+{
+    robots = robots.Select(r => r.Advance()).ToArray();
+    if (IsChristmasTreeCandidate(robots))
+    {
+        Clear();
+        WriteLine(new String(Enumerable.Repeat('-', Robot.Width).ToArray()));
+        var linesDic = robots.GroupBy(r => r.Y).ToDictionary(rg => rg.Key);
+        for (int y = 0; y < Robot.Height; y++)
+        {
+            Span<char> lineData = Enumerable.Repeat(' ', Robot.Width).ToArray();
+            if (linesDic.TryGetValue(y, out var lineGroup))
+            {
+                foreach (var r in lineGroup)
+                {
+                    lineData[r.X] = '*';
+                }
+            }
+            WriteLine(new string(lineData));
+        }
+        WriteLine(new String(Enumerable.Repeat('-', Robot.Width).ToArray()));
+        WriteLine($"\nIteration {i + 1} (does it look like a Christmas tree?)");
+        WaitForKey();
+    }
+}
 public record Robot(int X, int Y, int Dx, int Dy)
 {
     public const int Width = 101;
     public const int Height = 103;
-    private static readonly Regex regInput = new Regex("p=(-?\\d+),(-?\\d+) v=(-?\\d+),(-?\\d+)");
+    private static readonly Regex regInput = new("p=(-?\\d+),(-?\\d+) v=(-?\\d+),(-?\\d+)");
     public static Robot Parse(string line)
     {
         var match = regInput.Match(line);
